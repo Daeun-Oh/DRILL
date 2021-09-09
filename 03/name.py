9695f7881025ba58ed1a07c0377d71d1b3cb8316
Daeun-Oh <annsomde@naver.com>
2021-09-09 18:22:58 +0900

    drill 03 완성
    2020180025 오다은

---
+++ 03/name.py
@@ -0,0 +1,52 @@
+# Writing My Name
+
+import turtle
+
+# 오
+turtle.penup()
+turtle.goto(-200, 100)
+turtle.pendown()
+turtle.circle(30)
+turtle.penup()
+turtle.right(90)
+turtle.forward(10)
+turtle.pendown()
+turtle.forward(40)
+turtle.right(90)
+turtle.forward(55)
+turtle.right(180)
+turtle.forward(110)
+
+# 다
+turtle.penup()
+turtle.goto(-20, 150)
+turtle.left(180)
+turtle.pendown()
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(90)
+turtle.left(90)
+turtle.forward(50)
+turtle.penup()
+turtle.goto(0, 160)
+turtle.pendown()
+turtle.goto(0, 50)
+turtle.goto(0, 105)
+turtle.goto(20, 105)
+
+# 은
+turtle.penup()
+turtle.goto(125, 110)
+turtle.pendown()
+turtle.circle(25)
+turtle.penup()
+turtle.goto(75, 85)
+turtle.pendown()
+turtle.forward(100)
+turtle.penup()
+turtle.goto(95, 65)
+turtle.pendown()
+turtle.goto(95, 35)
+turtle.goto(155, 35)
+
+turtle.exitonclick()
