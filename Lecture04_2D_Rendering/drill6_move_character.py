26d00f66fe9c9dd97afe9b720eabe449e90b74de
Daeun-Oh <annsomde@naver.com>
2021-09-16 18:02:05 +0900

    drill 06 완성
    2020180025 오다은

--- Lecture04_2D_Rendering/drill6_move_character.py
+++ Lecture04_2D_Rendering/drill6_move_character.py
@@ -11,31 +11,49 @@ character.draw_now(400, 90)
 
 x = 400
 y = 90
+cnt = 0
+   
 
 while(True):
-      if x < 800 and y == 90:
-            clear_canvas_now()
-            grass.draw_now(400, 30)
-            character.draw_now(x, y)
-            x += 2
-            delay(0.005)
-      elif x == 800 and y < 600:
-            clear_canvas_now()
-            grass.draw_now(400, 30)
-            character.draw_now(x, y)
-            y += 2
-            delay(0.005)
-      elif x > 0 and y == 600:
-            clear_canvas_now()
-            grass.draw_now(400, 30)
-            character.draw_now(x, y)
-            x -= 2
-            delay(0.005)
-      elif x == 0 and y > 90:
-            clear_canvas_now()
-            grass.draw_now(400, 30)
-            character.draw_now(x, y)
-            y -= 2
-            delay(0.005)
+      if cnt == 0 or cnt == 1:
+            if x < 800 and y == 90:
+                  if x == 400 and cnt ==1:
+                        cnt = 2
+                        continue
+                  clear_canvas_now()
+                  grass.draw_now(400, 30)
+                  character.draw_now(x, y)
+                  x += 2
+                  delay(0.005)
+            elif x == 800 and y < 600:
+                  clear_canvas_now()
+                  grass.draw_now(400, 30)
+                  character.draw_now(x, y)
+                  y += 2
+                  delay(0.005)
+            elif x > 0 and y == 600:
+                  clear_canvas_now()
+                  grass.draw_now(400, 30)
+                  character.draw_now(x, y)
+                  x -= 2
+                  delay(0.005)
+            elif x == 0 and y > 90:
+                  clear_canvas_now()
+                  grass.draw_now(400, 30)
+                  character.draw_now(x, y)
+                  y -= 2
+                  delay(0.005)
+                  cnt = 1
 
+      if cnt == 2:
+            for i in range(90, 90 + 360 + 1):
+                  if i == 90 + 360:
+                        x = 400
+                        y = 90
+                        cnt = 0
+                        continue
+                  clear_canvas_now()
+                  grass.draw_now(400, 30)
+                  character.draw_now(400 + 210 * math.cos(math.radians(i)), 300 + 210 * math.sin(math.radians(i)))
+                  delay(0.005)
 close_canvas()
