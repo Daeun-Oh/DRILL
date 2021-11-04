89227e7a77d8e3e197d484c9f26ce0375ce72e52
Daeun-Oh <annsomde@naver.com>
2021-11-04 15:04:12 +0900

    11주차 수업 내용, 코드

---
+++ Lecture11_Character_Controller/test_class.py
@@ -0,0 +1,42 @@
+class Player:
+	type = 'Player'
+
+	def __init__(self):
+		self.x = 100
+
+	def where(self):
+		print(self.x)
+
+player = Player()
+player.where()
+
+# 클래스 변수 사용
+print(Player.type)
+
+# 클래스 함수 호출
+# Player.where()	# Error
+Player.where(player)	# OK, player.where()과 같음.
+
+
+
+
+
+'''
+class Star:
+
+	type = 'Star'
+	x = 100
+
+	def change():
+		x = 200
+		print('x is ', x)
+
+print('x IS ', Star.x)	# OK
+Star.change()			# OK
+print('x IS ', Star.x)	# OK
+
+star = Star()			# OK
+# --> Star.change(star)	# python이 알아서 이렇게 해석함
+print('x IS ', star.x)	# OK
+star.change()			# Error
+'''
