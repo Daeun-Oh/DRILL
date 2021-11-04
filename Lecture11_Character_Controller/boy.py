5bc9bcda9e38925f59fd78a5914695449a797952
Daeun-Oh <annsomde@naver.com>
2021-11-04 15:45:12 +0900

    주석 추가

--- Lecture11_Character_Controller/boy.py
+++ Lecture11_Character_Controller/boy.py
@@ -8,18 +8,22 @@ Do activity: 특정 상태에 머무르는 동안 수행하는 일(반복될 수
 from pico2d import *
 
 # Boy Event
-RIGHT_DOWN, LEFT_DOWN, RIGHT_UP, LEFT_UP, SLEEP_TIMER = range(5)
+RIGHT_DOWN, LEFT_DOWN, RIGHT_UP, LEFT_UP, SLEEP_TIMER = range(5) # 0부터 3까지의 정수값이 차례로 할당됨.
 
 key_event_table = {
+    # Key는 (정수, 정수) tuple   # Value는 정수
     (SDL_KEYDOWN, SDLK_RIGHT): RIGHT_DOWN,
     (SDL_KEYDOWN, SDLK_LEFT): LEFT_DOWN,
     (SDL_KEYUP, SDLK_RIGHT): RIGHT_UP,
     (SDL_KEYUP, SDLK_LEFT): LEFT_UP
+    # 입력 키값 해석을 단순화시키고, 키입력을 단일이벤트로 만들기 위한 매핑
+    # 오른쪽 키가 눌렸다 / 왼쪽 키가 눌렸다 / 오른쪽 키가 떼어졌다 / 왼쪽 키가 떼어졌다
 }
 
 # Boy States
 class IdleState:
-    def enter(boy, event):
+    # Entry Action
+    def enter(boy, event):  # 이벤트 전달 받음
         if event == RIGHT_DOWN:
             boy.velocity += 1
         elif event == LEFT_DOWN:
@@ -30,9 +34,11 @@ class IdleState:
             boy.velocity += 1
         boy.timer = 1000
 
+    # Exit Action
     def exit(boy, event):
         pass
 
+    # Do Activity
     def do(boy):
         boy.frame = (boy.frame + 1) % 8
         boy.timer -= 1
@@ -55,7 +61,7 @@ class RunState:
             boy.velocity -= 1
         elif event == LEFT_UP:
             boy.velocity += 1
-        boy.dir = boy.velocity
+        boy.dir = boy.velocity  # 방향을 현재 방향으로 맞춰준다.
 
     def exit(boy, event):
         pass
@@ -90,6 +96,7 @@ class SleepState:
             boy.image.clip_composite_draw(boy.frame * 100, 200, 100, 100, -3.141592 / 2, '', boy.x + 25, boy.y - 25, 100, 100)
 
 next_state_table = {
+    # 모든 경우를 생각. ex) 깨어있는데 때리면?, 자고있는데 10초가 지나면?
     IdleState: {RIGHT_UP: RunState, LEFT_UP: RunState, RIGHT_DOWN: RunState, LEFT_DOWN: RunState, SLEEP_TIMER: SleepState},
     RunState: {RIGHT_UP: IdleState, LEFT_UP: IdleState, LEFT_DOWN: IdleState, RIGHT_DOWN: IdleState},
     SleepState: {LEFT_DOWN: RunState, RIGHT_DOWN: RunState, LEFT_UP: RunState, RIGHT_UP: RunState}
@@ -104,12 +111,12 @@ class Boy:
         self.velocity = 0
         self.frame = 0
         self.timer = 0
-        self.event_que = []
-        self.cur_state = IdleState
-        self.cur_state.enter(self, None)
+        self.event_que = []         # 이벤트 큐 초기화
+        self.cur_state = IdleState  # 현재 상태: IdleState
+        self.cur_state.enter(self, None) # entry action 실행
 
 
-    def change_state(self,  state):
+    def change_state(self, state):
         pass
 
     def add_event(self, event):
@@ -117,7 +124,9 @@ class Boy:
 
 
     def update(self):
-        self.cur_state.do(self)
+        self.cur_state.do(self) # 현재 상태의 Do Activity 수행
+
+        # 이벤트가 있으면 현재 상태의 Exit Action 수행. 다음 상태 계산. 다음 상테의 Entry Action 수행.
         if len(self.event_que) > 0:
             event = self.event_que.pop()
             self.cur_state.exit(self, event)
@@ -130,6 +139,7 @@ class Boy:
         debug_print('Velocity :' + str(self.velocity) + ' Dir:' + str(self.dir))
 
 
+    # 입력된 키와 눌린 상태를 해석해서 이벤트를 만들고 이벤트 큐에 추가
     def handle_event(self, event):
         if (event.type, event.key) in key_event_table:
             key_event = key_event_table[(event.type, event.key)]
